fbf01f7839bc67ea1a10ce2f9c9ab3c5aff50933
spammenotinoz <spammenotinoz@gmail.com>
2024-12-03 11:20:11 +1100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,14 +9,14 @@ const config: Config = {
 	favicon: "img/favicon.png",
 
 	// Set the production url of your site here
-	url: "https://openwebui.com",
+	url: "https://ultimateai.org",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "open-webui", // Usually your GitHub org/user name.
+	organizationName: "spammenotinoz", // Usually your GitHub org/user name.
 	projectName: "docs", // Usually your repo name.
 
 	onBrokenLinks: "throw",
